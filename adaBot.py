50f7203a8de6afa9c446dc6588a4f965f40d7721
Georgina Sanchez <cpgeorgina.sanchez@gmail.com>
2020-01-02 10:22:54 -0800

    libraries imported

---
+++ adaBot.py
@@ -0,0 +1,13 @@
+import io #expects and produces str objects
+import random
+import string # to process standard python strings
+import warnings
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import warnings
+warnings.filterwarnings('ignore')
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+
