38c6d595a2f04d6f9e53d2a30ebdbe0cfe339b1a
Georgina Sanchez <cpgeorgina.sanchez@gmail.com>
2020-01-07 19:24:06 -0800

    fixed bug punctuation comming from the audio

--- adaBot.py
+++ adaBot.py
@@ -12,6 +12,7 @@ from nltk.stem import WordNetLemmatizer
 import azure.cognitiveservices.speech as speechsdk
 import os
 from dotenv import load_dotenv
+import azure.cognitiveservices.speech as speechsdk
 
 load_dotenv()
 
@@ -19,6 +20,7 @@ key = os.getenv("SPEECH_KEY")
 region = os.getenv("SERVICE_REGION")
 
 speech_config = speechsdk.SpeechConfig(subscription=key, region=region)
+speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config)
 
 with open('adaInfo.txt','r', encoding='utf8', errors ='ignore') as adaInfo:
     adaText = adaInfo.read().lower()
@@ -36,12 +38,11 @@ remove_punct_dict = { ord(punct): None for punct in string.punctuation }
 def LemNormalize(text):
     return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict))) #lowercase, remove punctuation, create tokens, and lemmatize
 
-
 # Creates a speech synthesizer using the default speaker as audio output.
 speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config)
 
 # Keyword Matching
-GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up","hey",)
+GREETING_INPUTS = ("hello.", "hi.", "greetings.", "sup.", "what's up.","hey.",)
 GREETING_RESPONSES = ["hi", "hey", "hi there", "hello"]
 
 def greeting(sentence):
@@ -61,36 +62,33 @@ def response(user_response):
     flat.sort()
     req_tfidf = flat[-2]
     if(req_tfidf==0):
-        ada_response=ada_response+"I am sorry! I don't understand you"
-        return speech_synthesizer.speak_text_async(ada_response).get()
+        ada_response = ada_response + "I am sorry! I don't understand you"
+        # Synthesizes the received text to speech.
+        # The synthesized speech is expected to be heard on the speaker with this line executed.
+        return speech_synthesizer.speak_text_async(ada_response + "I am sorry! I don't understand you").get()
     else:
-        ada_response = ada_response+sent_tokens[idx]
+        ada_response = ada_response + sent_tokens[idx]
+        # Synthesizes the received text to speech.
+        # The synthesized speech is expected to be heard on the speaker with this line executed.
         return speech_synthesizer.speak_text_async(ada_response).get()
 
 i=True
-print("Ada Robot: My name is AdaRobot and my pronouns are she her. I will answer your questions about Ada Developers Academy. If you want to exit, type Bye!")
-result = speech_synthesizer.speak_text_async("My name is AdaRobot and my pronouns are she and her. I will answer your questions about Ada Developers Academy. If you want to exit, type Bye!").get()
+result = speech_synthesizer.speak_text_async("My name is AdaRobot and my pronouns are she and her. I will answer your questions about Ada Developers Academy. If you want to exit, say thanks or thank you").get()
 while(i==True):
-    user_response = input()
-    user_response=user_response.lower()
-    # Synthesizes the received text to speech.
-    # The synthesized speech is expected to be heard on the speaker with this line executed.
-    result = speech_synthesizer.speak_text_async(user_response).get()
-    if(user_response!='bye'):
-        if(user_response=='thanks' or user_response=='thank you' ):
+    user_input = speech_recognizer.recognize_once()
+    user_response = user_input.text
+    user_response = user_response.lower()
+    if(user_response!='bye.'):
+        if(user_response=='thanks.' or user_response=='thank you.'):
             i=False
-            print("Ada Robot: You are welcome")
-            result = speech_synthesizer.speak_text_async("Ada Robot: You are welcome").get()
+            result = speech_synthesizer.speak_text_async("You are welcome! Thanks for comming to our presentation and for supporting Ada Developers Academy!").get()
         else:
             if(greeting(user_response)!=None):
-                print("Ada Robot: "+greeting(user_response))
                 result = speech_synthesizer.speak_text_async(greeting(user_response)).get()
             else:
-                print("Ada Robot: ", end=" ")
-                print(response(user_response))
+                response(user_response)
                 sent_tokens.remove(user_response)
     else:
         i=False
-        print("Ada Robot: Bye!")
-        result = speech_synthesizer.speak_text_async("Ada Robot: Bye!").get()
+        result = speech_synthesizer.speak_text_async("Bye! Thanks for comming to our presentation and for supporting Ada Developers Academy!").get()
         
