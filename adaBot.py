da88c5db0d94d16358a83b9348358d4deb1f2a78
Georgina Sanchez <cpgeorgina.sanchez@gmail.com>
2020-01-14 22:00:02 -0800

    Ada robot can answer questions about the user

--- adaBot.py
+++ adaBot.py
@@ -83,9 +83,9 @@ def create_response(user_response, text):
         flat.sort()
         req_tfidf = flat[-2]
         if(req_tfidf==0):
-            ada_response = ada_response + "I am sorry! I can't help you with that question try to ask me about the mission, inclusivity, Jump Start, etc."
+            ada_response = ada_response + "I am sorry! I can't help you with that question. Try to ask me about the mission, inclusivity, Jump Start, etc."
             sent_tokens.remove(user_response)
-            return (idx, ada_response)
+            return (-1, ada_response)
         else:
             ada_response = ada_response + sent_tokens[idx]
             sent_tokens.remove(user_response)
@@ -102,14 +102,37 @@ while(i==True):
     if(user_response!='bye.'):
         if(user_response=='thanks.' or user_response=='thank you.'):
             i=False
-            result = speech_synthesizer.speak_text_async("You are welcome! Thanks for comming to our presentation and for supporting Ada Developers Academy!").get()
-        elif (user_response.split(',')=='how old am i?'):
-            result = speech_synthesizer.speak_text_async("you look like" + str(round(info[0]["faceAttributes"]["age"])) + "you look very young!").get()
+            speech_synthesizer.speak_text_async("You are welcome! Thanks for comming to our presentation and for supporting Ada Developers Academy!").get()
         else:
             idx, response_text = create_response(user_response, 'adaInfo.txt')
-            # Synthesizes the received text to speech.
-            # The synthesized speech is expected to be heard on the speaker with this line executed.
-            speech_synthesizer.speak_text_async(response_text).get()
+            if(idx == -1):
+                idx, response_text = create_response(user_response, 'userQuestions.txt')
+                if(idx == 0):
+                    speech_synthesizer.speak_text_async("you look like" + str(round(info[0]["faceAttributes"]["age"])) + "you look very young!").get()
+                elif(idx == 1):
+                  if(info[0]["faceAttributes"]["makeup"]["eyeMakeup"]) and info[0]["faceAttributes"]["makeup"]["eyeMakeup"]:
+                      speech_synthesizer.speak_text_async("your eye makeup and lipstick are lovely!")
+                  elif(info[0]["faceAttributes"]["makeup"]["eyeMakeup"]):
+                      speech_synthesizer.speak_text_async("your eye makeup is wonderful!")
+                  elif(info[0]["faceAttributes"]["makeup"]["lipstick"]):
+                      speech_synthesizer.speak_text_async("your lipstick color is beautiful!")
+                  else:
+                      speech_synthesizer.speak_text_async("it seems that you are not wearing makeup!")
+                elif (idx == 2):
+                  if (info[0]["faceAttributes"]["accessories"]):
+                      speech_synthesizer.speak_text_async("your " + str(info[0]["faceAttributes"]["accessories"][0]["type"]) + " are so cool!").get()
+                  else:
+                    speech_synthesizer.speak_text_async("I am sorry! I can't help you with that question. Try to ask me about the mission, inclusivity, Jump Start, etc.").get()
+                elif (idx == 3):
+                  max_emotion = Keymax = max(info[0]["faceAttributes"]["emotion"], key=info[0]["faceAttributes"]["emotion"].get)
+                  emotions = { "anger": "angry","contempt": "contempty", "disgust": "disgusty","fear": "scared", "happiness": "happy", "neutral": "neutral", "sadness": "sad", "surprise": "surprised"} 
+                  speech_synthesizer.speak_text_async("you are " + emotions[max_emotion]).get()
+                else:
+                  speech_synthesizer.speak_text_async("I am sorry! I can't help you with that question. Try to ask me about the mission, inclusivity, Jump Start, etc.").get()
+            else:
+                # Synthesizes the received text to speech.
+                # The synthesized speech is expected to be heard on the speaker with this line executed.
+                speech_synthesizer.speak_text_async(response_text).get()
 
     else:
         i=False
