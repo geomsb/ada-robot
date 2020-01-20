3ebbeda3663569bc67adb8f4cc33abdf5abd2c36
geomsb <cpgeorgina.sanchez@gmail.com>
2020-01-20 03:48:46 +0000

    leds API

--- api/raspApi.py
+++ api/raspApi.py
@@ -1,5 +1,6 @@
 import flask
 import RPi.GPIO as GPIO
+from flask import request
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -8,10 +9,51 @@ red_pin   = 11
 green_pin = 13
 blue_pin  = 15
 
-@app.route('/', methods=['GET'])
-def led_on():
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(red_pin, GPIO.OUT)
-    GPIO.output(red_pin, GPIO.LOW)
 
-app.run()
+def turn_on(pin):
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, GPIO.LOW)
+
+def turn_off(pin):
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, GPIO.HIGH)
+
+def white_on():
+    turn_on(red_pin)
+    turn_on(green_pin)
+    turn_on(blue_pin)
+
+def all_off():
+    turn_off(red_pin)
+    turn_off(green_pin)
+    turn_off(blue_pin)
+
+def blue_on():
+    turn_on(blue_pin)
+    turn_off(red_pin)
+    turn_off(green_pin)
+
+def magenta_on():
+    turn_on(red_pin)
+    turn_on(blue_pin)
+    turn_off(green_pin)
+
+
+@app.route('/', methods=['POST'])
+def led_on():
+    content = request.json
+    if(content["color"] == "white"):
+        white_on()
+    elif(content["color"] == "blue"):
+        blue_on()
+    elif(content["color"] == "magenta"):
+        magenta_on()
+    elif(content["color"] == "off"):
+        all_off()
+    return content["color"]
+    
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
